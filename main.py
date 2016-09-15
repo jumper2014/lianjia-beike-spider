7e572e7aef44e7d3f62ee5906f8ad5583ee3a292
YueTian Zeng <zengyuetian@YueTians-MacBook-Pro.local>
2016-09-15 12:44:00 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 
 import requests
-from bs4 import BeautifulSoup
-import lxml
+from BeautifulSoup import BeautifulSoup
+from lxml import etree
 import re
 
+base_url = "http://sh.lianjia.com"
 
 if __name__ == "__main__":
     second_hand_url = "http://sh.lianjia.com/ershoufang/"
@@ -12,6 +13,16 @@ if __name__ == "__main__":
     response = requests.get(second_hand_url)
     html = response.content
 
-    soup = BeautifulSoup(html)
+    # soup = BeautifulSoup(html)
+    root = etree.HTML(html)
+    print root
+
+    xpath_str = '//*[@id="filter-options"]/dl[1]/dd/div/a'
+    links = root.xpath(xpath_str)
+    # print links
+    for link in links:
+        print base_url + link.attrib['href']
+
+
 
 
