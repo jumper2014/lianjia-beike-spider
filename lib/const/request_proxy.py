43651f6d18da94cf6f5f9a6212d79ca0ef83b2d4
zyt <ijumper@163.com>
2018-10-06 17:45:57 +0800

    add proxy ip function

---
+++ lib/const/request_proxy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: zengyuetian
+
+from bs4 import BeautifulSoup
+import requests
+import random
+from lib.const.request_headers import create_headers
+
+proxys_src = []
+proxys = []
+
+
+def spider_proxyip(num=10):
+    try:
+        url = 'http://www.xicidaili.com/nt/1'
+        req = requests.get(url, headers=create_headers())
+        source_code = req.content
+        print(source_code)
+        soup = BeautifulSoup(source_code, 'lxml')
+        ips = soup.findAll('tr')
+
+        for x in range(1, len(ips)):
+            ip = ips[x]
+            tds = ip.findAll("td")
+            proxy_host = "{0}://".format(tds[5].contents[0]) + tds[1].contents[0] + ":" + tds[2].contents[0]
+            proxy_temp = {tds[5].contents[0]: proxy_host}
+            proxys_src.append(proxy_temp)
+            if x >= num:
+                break
+    except Exception as e:
+        print ("spider_proxyip exception:")
+        print (e)
+
+
+if __name__ == '__main__':
+    spider_proxyip(10)
+    print(proxys_src)
