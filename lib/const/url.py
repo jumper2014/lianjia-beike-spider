6fa711aaaad71ac0763504a3b4e32a1c37ffd6ee
zyt <ijumper@163.com>
2018-10-22 22:03:48 +0800

    delete useless code file

--- lib/const/url.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-# author: zengyuetian
-#
-# LIANJIA_SH_BASE_URL = "http://sh.lianjia.com"
-# ERSHOUFANG_BASE_URL = "http://sh.lianjia.com/ershoufang"
-# SH_XIAOQU_BASE_URL = 'http://sh.lianjia.com/xiaoqu'
-
