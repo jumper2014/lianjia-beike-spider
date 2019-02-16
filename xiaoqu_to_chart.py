9c9b2dbd428846c818fcf26c9707a6b58f2b0e40
zyt <ijumper@163.com>
2019-02-16 17:31:42 +0800

    handle the situation for webbrowser not installed

--- xiaoqu_to_chart.py
+++ xiaoqu_to_chart.py
@@ -8,21 +8,28 @@
 
 import pandas as pd
 from pyecharts import Bar
-import webbrowser as web
+
 import os
 import time
 from lib.utility.version import PYTHON_3
 
 if __name__ == '__main__':
     try:
-        if PYTHON_3:
-            os.system("ps aux | grep python | grep http.server | grep -v grep | awk '{print $2}' | xargs kill")
-            os.system("python -m http.server 8080 & > /dev/null 2>&1 ")
-        else:
-            os.system("ps aux | grep python | grep SimpleHTTPServer | grep -v grep | awk '{print $2}' | xargs kill")
-            os.system("python -m SimpleHTTPServer 8080 & > /dev/null 2>&1 ")
+        import webbrowser as web
+        auto_browse = True
     except Exception as e:
-        print(e)
+        auto_browse = False
+
+    if auto_browse:
+        try:
+            if PYTHON_3:
+                os.system("ps aux | grep python | grep http.server | grep -v grep | awk '{print $2}' | xargs kill")
+                os.system("python -m http.server 8080 & > /dev/null 2>&1 ")
+            else:
+                os.system("ps aux | grep python | grep SimpleHTTPServer | grep -v grep | awk '{print $2}' | xargs kill")
+                os.system("python -m SimpleHTTPServer 8080 & > /dev/null 2>&1 ")
+        except Exception as e:
+            print(e)
 
     # 注意，已经将分割符号转换成分号，因为有的小区名中有逗号
     df = pd.read_csv("xiaoqu.csv", encoding="utf-8", sep=";")
@@ -62,9 +69,10 @@ if __name__ == '__main__':
     bar.add("区县均价排名", districts, prices, is_stack=True, is_label_show=True, xaxis_interval=0, xaxis_rotate=45)
     bar.render(path="district.html")
 
-    web.open("http://localhost:8080/xiaoqu.html", new=0, autoraise=True)
-    web.open("http://localhost:8080/district.html", new=0, autoraise=True)
-    # 确保页面打开
-    time.sleep(15)
+    if auto_browse:
+        web.open("http://localhost:8080/xiaoqu.html", new=0, autoraise=True)
+        web.open("http://localhost:8080/district.html", new=0, autoraise=True)
+        # 确保页面打开
+        time.sleep(15)
 
 
